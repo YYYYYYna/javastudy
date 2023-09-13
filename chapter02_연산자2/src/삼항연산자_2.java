558d7515df4db3a12906bac4bd385dab16b17f8e
sist <sist@DESKTOP-BMHMOT7>
2023-09-13 17:01:47 +0900

    333

--- chapter02_연산자2/src/삼항연산자_2.java
+++ chapter02_연산자2/src/삼항연산자_2.java
@@ -1,8 +1,16 @@
-
+/*             if           else 처럼 사용함
+ *          --true--<조건>--false--
+ *          |                    |
+ *          ----------------------
+ *                    |
+ *                 [결과값]
+ *                 
+ *   주로 2가지 경우의 수가 나오면 사용함
+ *   ex) 아이디중복체크, 로그인, ID입력, ID/PW찾기
+ */
 public class 삼항연산자_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
---
+++ chapter02_연산자2/src/예제문제0913.java
@@ -0,0 +1,36 @@
+
+public class 예제문제0913 {
+
+	public static void main(String[] args) {
+	    int num1=(int)(Math.random()*100)+1;
+	    int num2=(int)(Math.random()*100)+1;
+	    int re1=num1-num2;
+	    int re2=num1*num2;
+	    System.out.println("num1 : "+num1+" ,num2 : "+num2);
+	    System.out.println("뺄셈 : "+re1);
+	    System.out.println("곱셈 : "+re2);
+	    
+	    int num4=(int)(Math.random()*4)+1;
+	    int num5=(int)(Math.random()*4)+1;
+	    int num6=(int)(Math.random()*4)+1;
+	    int re3=num4*num5+num6;
+	    System.out.println("num4 : "+num4+" ,num5 : "+num5+" ,num6 : "+num6);
+	    System.out.println("결과값 : "+re3);
+	    
+	    int num7=(int)(Math.random()*100)+1;
+	    int num8=(int)(Math.random()*100)+1;
+	    int re4=num7%num8;
+	    System.out.println("num7 : "+num7+" ,num8 : "+num8);
+	    System.out.println("결과값 : "+re4);
+	    
+	    int a = 5;
+	    int b = 5;
+	    System.out.println("a와 b가 같은가요?" + (a==b));
+	    System.out.println("a가 b보다 큰가요?" + (a>b));
+	    System.out.println("a가 b보다 작은 가요 ?" + (a<b));
+	    System.out.println("a가 b와 같은면서 3보다 큰가요?" + (a==b && a>3));
+	    System.out.println("a가 50이 아닌가요? " + (a!=50));
+
+	}
+
+}
