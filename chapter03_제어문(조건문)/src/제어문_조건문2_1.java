ea5f9c4c569febc4d79ff492da2f72dc818b7e5f
sist <sist@DESKTOP-BMHMOT7>
2023-09-18 09:55:52 +0900

    9:55

---
+++ chapter03_제어문(조건문)/src/제어문_조건문2_1.java
@@ -0,0 +1,13 @@
+/*
+ *   if~else
+ *   
+ *   예상되는 결과값이 두개로 나눠져서 나오는 경우 => if~else
+ */
+public class 제어문_조건문2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
