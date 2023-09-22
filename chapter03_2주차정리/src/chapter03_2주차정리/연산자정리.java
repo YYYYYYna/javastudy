ce6ef8aab4df089ee8b5a6b2c3d6de417e6b19ce
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 <officeight@naver.com>
2023-09-22 11:00:24 +0900

    11:00

---
+++ chapter03_2주차정리/src/chapter03_2주차정리/연산자정리.java
@@ -0,0 +1,19 @@
+/*
+ *     연산자
+ *     -----
+ *     => 결과값 (데이터형 사용)
+ *     1) 피연산자(연산대상) / 연산자
+ *        일항연산자 : 증감연산자(a++) , 부정연산자(!a) , 형변환연산자(type)10
+ *        이항연산자 : 산술연산자(a+b) , 비교연산자 , 논리연산자, 대입연산자
+ *        삼항연산자 : (조건)?값:값
+ */
+package chapter03_2주차정리;
+
+public class 연산자정리 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
