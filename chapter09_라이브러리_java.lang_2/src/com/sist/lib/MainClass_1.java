70d349090fd736cdab9b8a626219a04078ce6afe
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 <officeight@naver.com>
2023-10-24 11:04:16 +0900

    10.24 11:04

---
+++ chapter09_라이브러리_java.lang_2/src/com/sist/lib/MainClass_1.java
@@ -0,0 +1,24 @@
+package com.sist.lib;
+
+/*
+ *    Wrapper : 기본 데이터형을 클래스화
+ *      => 기본 데이터형에 기능을 부여해서 쉽게 사용이 가능
+ *      int ==> Integer
+ *      long ==> Long
+ *      double ==> Double
+ *      boolean ==> Boolean
+ *      --------------------웹에서 주로 사용
+ *      
+ *      => 웹/윈도우 => 모든 데이터값 문자열
+ *                   ---------------
+ *                   문자열을 정수,실수,논리형
+ */
+
+public class MainClass_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
