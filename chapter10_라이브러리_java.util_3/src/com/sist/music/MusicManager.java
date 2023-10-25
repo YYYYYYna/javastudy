35294c7af4d06e5ec6f17ed093c42409cafeb2c8
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 <officeight@naver.com>
2023-10-25 17:55:36 +0900

    10.25 17:55

--- chapter10_라이브러리_java.util_3/src/com/sist/music/MusicManager.java
+++ chapter10_라이브러리_java.util_3/src/com/sist/music/MusicManager.java
@@ -1,5 +1,90 @@
 package com.sist.music;
+//여기가 DAO부분~~ 기능수행부분임
+/*
+ *  크롤링 => 데이터를 모아서 메모리에 저장
+ *                     ---------
+ *                     1)배열
+ *                     2)컬렉션
+ *  크롤링 => 파일
+ *  크롤링 => 오라클
+ *  ----------------> 브라우저에 출력
+ */
+import java.util.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 public class MusicManager {
-
+	
+	//현재 뮤직 전체 데이터를 가지고 있음
+	//클라이언트 접속에게 공유 => static
+	
+	//1. 데이터 모으는데 하나의 값을 공유할때는 무조건 static
+	private static ArrayList<Music> list=new ArrayList<Music>();
+	
+	//2. 실행과 동시에 여러데이터 저장(크롤링) ==> static블록이용함
+	static 
+	{
+		//네트워크와 관련되어 있으니 예외처리하기
+		try {                 //connet링크에서 가져와라(=get) 라는뜻
+			Document doc=Jsoup.connect("https://www.genie.co.kr/chart/top200").get();
+			//포스터 가져오기 => <img>
+			Elements poster=doc.select("table.list-wrap a.cover img");
+			//곡명 가져오기
+			Elements title=doc.select("table.list-wrap td.info a.title");
+			//가수별 가져오기
+			Elements singer=doc.select("table.list-wrap td.info a.artist");
+			//앨범 가져오기
+			Elements album=doc.select("table.list-wrap td.info a.albumtitle");
+			
+			for(int i=0;i<poster.size();i++)
+			{
+				Music music=new Music();
+				music.setRank(i+1);
+				music.setPoster("http:"+poster.get(i).attr("src"));//attr는 src의 값을 가져올때 쓰는 테그
+				music.setTitle(title.get(i).text());
+				music.setSinger(singer.get(i).text());
+				music.setAlbum(album.get(i).text());
+				
+				//가져온 데이터 한곳에 모으기
+				list.add(music);
+			}
+		}catch(Exception ex) {ex.printStackTrace();}
+	}
+	
+	//3. 기능(메소드) 생성
+	//3-1) 목록출력 
+	public ArrayList<Music> musicAllData()
+	{
+		return list;
+	}
+	
+	//3-2) 상세보기 ==> 1개만나옴=Music
+	public Music musicDetailData(int no)
+	{
+		Music music=new Music();
+		for(Music m:list)
+		{
+			if(m.getRank()==no)
+			{
+				music=m;
+				break;
+			}
+		}
+		return music;
+	}
+	//3-3) 검색 ==> 여러개만나옴=ArrayList<Music>
+	public ArrayList<Music> MusicFindData(String title)
+	{
+		ArrayList<Music> arr=new ArrayList<Music>();
+		for(Music m:list)
+		{
+			if(m.getTitle().contains(title))
+			{
+				arr.add(m);
+			}
+		}
+		return arr;
+	}
+	
 }
--- chapter10_라이브러리_java.util_3/src/com/sist/music/UserMain.java
+++ chapter10_라이브러리_java.util_3/src/com/sist/music/UserMain.java
@@ -1,10 +1,106 @@
 package com.sist.music;
 
-public class UserMain {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
 
+import javax.swing.table.*;
+
+public class UserMain extends JFrame implements ActionListener{
+	
+	JTextField tf;
+	JButton b,b2;
+	JTable table;
+	DefaultTableModel model;
+	MusicManager mm=new MusicManager(); //포함클래스
+	
+	//하나의 값을 공유할때는 무조건 static
+	public UserMain() 
+	{
+		tf=new JTextField(10);
+		b=new JButton("검색");
+		b2=new JButton("목록");
+		JPanel p=new JPanel();
+		p.add(tf); p.add(b); p.add(b2);
+		
+		String[] col= {"순위","곡명","가수명"};
+		String[][] row=new String[0][3];
+		
+		model=new DefaultTableModel(row,col);
+		table=new JTable(model);
+		JScrollPane js=new JScrollPane(table);
+		
+		//윈도우에 올리기
+		add("North",p);
+		add("Center",js);
+		
+		musicPrint();
+		setSize(800,600);
+		setVisible(true);
+		
+		b.addActionListener(this);
+		b2.addActionListener(this);
+		tf.addActionListener(this); //this안에 있는걸 실행한다는뜻
+	}
+	
+	public void musicPrint() 
+	{
+		ArrayList<Music> list=mm.musicAllData();
+		
+		for(int i=model.getRowCount()-1;i>=0;i--)//0부터 지우면 하나가 남아서 뒤에서부터 지워나감
+		{
+			model.removeRow(i);//한번 지움
+		}
+		
+		for(Music m:list)
+		{                 //int => String으로 변환 (형변환불가능)
+			String[] data={String.valueOf(m.getRank()),m.getTitle(),m.getSinger()};//데이터3개를
+			model.addRow(data);//한줄씩 추가
+		}
+	}
+	
+	public void musicFindData(String title) 
+	{
+		ArrayList<Music> list=mm.MusicFindData(title);
+		
+		for(int i=model.getRowCount()-1;i>=0;i--)//0부터 지우면 하나가 남아서 뒤에서부터 지워나감
+		{
+			model.removeRow(i);//한번 지움
+		}
+		
+		for(Music m:list)
+		{                 //int => String으로 변환 (형변환불가능)
+			String[] data={String.valueOf(m.getRank()),m.getTitle(),m.getSinger()};//데이터3개를
+			model.addRow(data);//한줄씩 추가
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new UserMain(); 
+	}
 
+	//여기가 버튼 눌렀을때 행동하는 부분
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==b||e.getSource()==tf)//버튼을누르거나? 엔터를눌러서! 검색실행
+		{
+			//검색출력
+			String title=tf.getText();
+			if(title.length()<1)//~예외처리부~
+			{
+				JOptionPane.showMessageDialog(this, "검색어를 입력하세요");//showMessageDialog=alt경고창
+				tf.requestFocus();
+				return;
+			}
+			musicFindData(title);
+		}
+		else if(e.getSource()==b2)
+		{
+			//목록출력
+			musicPrint();
+		}
+		
 	}
 
 }
